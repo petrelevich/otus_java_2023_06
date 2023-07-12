579bd36df0b90b1f606279342a1089cb7ae2fba2
stvort <stvort@mail.ru>
2023-07-12 20:23:11 +0400

    L03-qa added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,8 @@ include("L02-gradle2")
 include("L02-gradle2-libApi")
 include("L02-gradle2-libApiUse")
 
+include("L03-qa")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
