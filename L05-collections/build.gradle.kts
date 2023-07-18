f7e0a5a8d48ebfc5d5e8d5988e3fee9bff7092f7
petrelevich <petrelevich@yandex.ru>
2023-07-18 20:12:27 +0300

    L05 collections

---
+++ L05-collections/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    testImplementation ("org.openjdk.jmh:jmh-core")
+    testAnnotationProcessor ("org.openjdk.jmh:jmh-generator-annprocess")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+}
